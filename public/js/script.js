d58756f40be09d6d534a9c5cedd7315ee0c4c648
BHAVIT-SINGLA <bhavithisar2001@gmail.com>
2022-07-01 15:34:41 +0530

    Commit final

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 let addIngredientsBtn = document.getElementById('addIngredientsBtn');
 let ingredientList = document.querySelector('.ingredientList');
 let ingredeintDiv = document.querySelectorAll('.ingredeintDiv')[0];
-
+//jdv
 addIngredientsBtn.addEventListener('click', function(){
   let newIngredients = ingredeintDiv.cloneNode(true);
   let input = newIngredients.getElementsByTagName('input')[0];
